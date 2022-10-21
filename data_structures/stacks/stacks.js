ef725a2313b2e04e6e9e351746bde4fd3f5147dd
Richard Walsh <richwalsh2@googlemail.com>
2022-10-21 21:19:02 +0100

    section 6 lesson 44 stack pop

---
+++ data_structures/stacks/stacks.js
@@ -0,0 +1,48 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.top = newNode;
+    this.length = 1;
+  }
+
+  //   ADD NODE TO TOP OF STACK
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.top = newNode;
+    } else {
+      newNode.next = this.top;
+      this.top = newNode;
+    }
+
+    this.length += 1;
+    return this;
+  }
+
+  //   REMOVE NODE FROM TOP OF STACK AND RETURN NODE
+  pop() {
+    if (this.length === 0) {
+      return undefined;
+    }
+    const temp = this.top;
+    this.top = this.top.next;
+    temp.next = null;
+    this.length -= 1;
+    return temp;
+  }
+}
+
+const myStack = new Stack(1);
+console.log(myStack);
+console.log(myStack.push(2));
+console.log(myStack.push(3));
+console.log(myStack.push(4));
+console.log(myStack.pop());
+console.log(myStack.pop());
