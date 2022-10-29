7b8f370576f854e95719cf168f142f511ac1c89c
Richard Walsh <richwalsh2@googlemail.com>
2022-10-29 18:31:48 +0100

    section 13 quick sort lesson 93

--- algorithms/mergeSort/mergeSort.js
+++ algorithms/mergeSort/mergeSort.js
@@ -2,6 +2,7 @@ const arr1 = [1, 3, 7, 8];
 const arr2 = [2, 4, 5, 6];
 
 const merge = (array1, array2) => {
+  console.log('merge');
   let combined = [];
   let i = 0;
   let j = 0;
@@ -28,4 +29,24 @@ const merge = (array1, array2) => {
   return combined;
 };
 
-console.log(merge(arr1, arr2));
+// console.log(merge(arr1, arr2));
+
+const newArray = [1, 9, 2, 8, 3, 7, 4, 6, 5, 0];
+const testArray = ['i', 'h', 'a', 'r', 'r', 'd', 'c'];
+console.log(newArray);
+
+const mergeSort = (array) => {
+  if (array.length === 1) {
+    return array;
+  }
+
+  let mid = Math.floor(array.length / 2);
+  let left = array.slice(0, mid);
+  let right = array.slice(mid);
+  console.log(left);
+
+  return merge(mergeSort(left), mergeSort(right));
+};
+
+console.log(mergeSort(newArray));
+console.log(mergeSort(testArray));
